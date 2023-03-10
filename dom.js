ffb8f868948b2bdadbc6b50176f649560074499c
sa07m <samiya90mohsin@gmail.com>
2023-03-10 22:06:43 +0530

    task

---
+++ dom.js
@@ -0,0 +1,12 @@
+// EXAMINE THE DOCUMENT OBJECT
+
+//console.dir(document);
+//console.log(document.domain);
+//console.log(document.URL);
+console.log(document.title); 
+//document.title = 123; // change the title
+console.log(document.title);
+var titleChange = document.getElementById('header-title');
+titleChange.style.border = 'dotted #000';
+var x = document.getElementById('main');
+
